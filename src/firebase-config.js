9d577f4dfb1a37ba03b7068cfc3d8e0282776231
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-09-02 19:46:21 +0100

    edited error

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,11 +5,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: "nutrifeed-app-fdcde.firebaseapp.com",
-   projectId: "nutrifeed-app-fdcde",
-   storageBucket: "nutrifeed-app-fdcde.appspot.com",
-   messagingSenderId: "278261515508",
-   appId: "1:278261515508:web:87efddee28d693cfa24aa1",
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
